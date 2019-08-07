64564bd59f667be297fd24f6ebb8e58ccee12292
Gideon Kosgei <mr.gkosgei@gmail.com>
2019-08-07 13:54:18 +0300

    added json error reporting

--- app.js
+++ app.js
@@ -44,9 +44,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // show the error message 
+  res.json( {
+    "status": (err.status || 500),
+    "error": res.locals.message
+  }
+    );
 });
 
 module.exports = app;
